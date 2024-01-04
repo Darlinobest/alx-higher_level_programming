58abb7a4f16f500858f689bb0d6c61c40635d57d
Onwuemebolam Darlington Kelechi <darlinobest@yahoo.com>
2024-01-04 05:51:18 -0800

    test task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[0:6]
+str = str.split()
+str = " ".join([str[5], str[6], str[12], str[0]])
 print(str)
