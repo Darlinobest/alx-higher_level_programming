e223db31997034287c4ba7b40fd0eab72857dd32
darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2024-01-16 18:51:44 -0800

    a program that prints numbers from 0 to 99 and separated by ,, followed by a space

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    print(f"{i:02}", end=", ")
-print("\n")
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
