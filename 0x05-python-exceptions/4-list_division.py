fa42b20d035879f71f9bb43792ddff2d8ad6792d
Darlington Onwuemebolam <darlinobest@yahoo.com>
2024-04-14 15:37:13 -0700

    a function that raises a type exception.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,15 +9,16 @@ def list_division(my_list_1, my_list_2, list_length):
     for x in range(0, list_length):
         try:
             result = my_list_1[x] / my_list_2[x]
+        except TypeError:
+            print("wrong type")
+            result = 0
         except ZeroDivisionError:
             print("division by 0")
-            result = 0
-        except (TypeError):
-            print("wrong type")
             result = 0
         except IndexError:
             print("out of range")
             result = 0
+            break
         finally:
             result_list.append(result)
     return result_list
