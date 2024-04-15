5c3f3619cafaa528f279450d476d723792901abe
Darlington Onwuemebolam <darlinobest@yahoo.com>
2024-04-14 17:35:11 -0700

    a function that prints an integer.

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}\n".format(value), end="")
+        return True
+    except Exception as e:
+        print(f"Exception: {e}")
+        return False
