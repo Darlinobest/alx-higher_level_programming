6bd870ec9a141ae466cbda4381779c3eb20bce88
Darlington Onwuemebolam <darlinobest@yahoo.com>
2024-04-16 09:26:52 -0700

    a class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+# 1-square.py
+# Darlington K Onwuemebolam
+""" Class representation of Square"""
+
 
 class Square:
+    """Private attribute of size definition"""
     def __init__(self, size):
         self.__size = size
