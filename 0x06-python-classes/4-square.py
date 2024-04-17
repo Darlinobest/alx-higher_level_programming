b32d057ee180dac06289c16cc1b8b9186c810c09
Darlington Onwuemebolam <darlinobest@yahoo.com>
2024-04-17 08:49:55 -0700

    a class Square that defines a square by: (based on 3-square.py)

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# 4-square.py
+# Darlington K Onwuemebolam
+""" Class definition of Square """
+
+
+class Square:
+    """ Represent the class square """
+    def __init__(self, size=0):
+        """ Initialize the square with the given size """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Getter method for the size attribute """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter method for the size attribute """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ Return the area of the square """
+        return self.__size ** 2
