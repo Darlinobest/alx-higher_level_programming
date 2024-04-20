fe907503f32722635998c981b59f4e678e21dd11
Darlington Onwuemebolam <darlinobest@yahoo.com>
2024-04-19 23:54:08 -0700

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,7 +13,7 @@ class Square:
     @property
     def size(self):
         """Get the size of the square."""
-        return self.__seize
+        return self.__size
 
     @size.setter
     def size(self, value):
